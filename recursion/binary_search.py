fac8702237d4ce9744e242ebe7c3e698ee859d97
wangonya <kwangonya@gmail.com>
2020-06-30 21:41:15 +0300

    Update binary_search.py

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -1,14 +1,29 @@
 def binary_search(data, target, low, high):
-    if low > high:
-        return False  # interval is empty - no match
-    else:
-        mid = (low+high) // 2
-        if target == data[mid]:  # found match
-            return True
-        elif target < data[mid]:
-            # recur to the portion left of the middle
-            return binary_search(data, target, low, mid-1)
-        else:
-            # recur to the portion right of the middle
-            return binary_search(data, target, mid+1, high)
+    """uses recursive binary search algorithm to find
+    and return the target in entered data
 
+    assumes data is sorted"""
+
+    if low > high:  # empty list - no match (base case)
+        print("target not in list")
+    else:
+        mid = (low+high)//2
+        print(f"mid = {mid}")
+        if target == data[mid]:  # found target
+            print(f"data == {data}")
+            print(f"target == {target}")
+            print(f"found! {data[mid]}")
+        elif data[mid] > target:  # recurse on left side
+            high = mid-1
+            binary_search(data, target, low, high)
+        elif data[mid] < target:  # recurse on right side
+            low = mid+1
+            binary_search(data, target, low, high)
+
+if __name__ == '__main__':
+    data = list(map(int, input().strip().split(' ')))
+    target = int(input().strip())
+    low = 0
+    high = len(data)-1
+
+    binary_search(data, target, low, high)
