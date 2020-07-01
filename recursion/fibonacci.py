23d2d527e14048e479e52f16bd5044ad58770c7d
wangonya <kwangonya@gmail.com>
2020-07-01 19:53:00 +0300

    add prints for debugging

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-#O(n!)
+# O(n!)
 def bad_fibonacci(n):
     """return the nth fib number"""
     if n <= 1:
@@ -10,11 +10,15 @@ def bad_fibonacci(n):
     else:
         return bad_fibonacci(n - 2) + bad_fibonacci(n - 1)
 
-#O(n)
+
+# O(n)
 def good_fibonacci(n):
     """return a pair of fib numbers, f(n) and f(n-1)"""
     if n <= 1:
+        print(f'n ==> {n}')
         return (n, 0)
     else:
+        print(f'n ==> {n}')
         (a, b) = good_fibonacci(n - 1)
+        print(f'a,b ==> {(a,b)}')
         return (a + b, a)
